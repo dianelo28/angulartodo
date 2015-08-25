07631f1b84d4c9040e80ef655592fe246864c66b
Diane Lo <Diane@Dianes-MacBook-Pro.local>
2015-08-25 09:04:17 -0700

    template showing up

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-angular.modules('starter', [ngRoute])
+angular.module('starter', ['ngRoute'])
 
 	.config(['$routeProvider',
-	   function($routeProvider) {
-	     $routeProvider.
-	       when('/', {
-	         templateUrl: 'templates/todos.html',
-	         controller: 'TodoCtrl'
-	       })
-	       otherwise({
-	         redirectTo: '/'
-	       });
-	       
-	.controller("MainCtrl", ['$scope', '$rootScope', 
-		function($scope, $rootScope){
-			$scope.term = "cellar door";
-			$scope.showAlert = function(){
-				alert($scope.term);
-		};
+	function($routeProvider) {
+     $routeProvider.
+       when('/', {
+         templateUrl: 'templates/todos.html',
+         controller: 'TodoCtrl'
+       }).
+       otherwise({
+         redirectTo: '/'
+       });
 	}])
-	.controller("ToDoCtrl",['$scope', 
-		function($scope){
+
+	// .controller("MainCtrl", ['$scope', '$rootScope', 
+	// 	function($scope, $rootScope){
+	// 		$scope.term = "cellar door";
+	// 		$scope.showAlert = function(){
+	// 			alert($scope.term);
+	// 	}
+	.controller("ToDoCtrl",['$scope', '$rootScope',
+		function($scope, $rootScope){
 			$scope.todos = [];
 			$scope.addToDo = function(){
-				$scope.todo.completed = false;
 				$scope.todos.push($scope.todo);
 				$scope.todo = "";
-		}
+			};
 			$scope.remove = function(todo){
 				var index = $scope.todos.indexOf(todo);
 				$scope.todos.splice(index, 1)
-		}
+			};
 			$scope.date = new Date()
-
-	}])
+	}]);
--- controllers.js
+++ controllers.js
@@ -1,3 +1,2 @@
-angular.modules('starter', [])
 
 	
